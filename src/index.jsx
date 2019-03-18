d98edbfb6638099c825b1fc40285324b8d51c8bf
Daniel-Stafford <stafford.daniel@gmail.com>
2019-03-18 16:27:18 +0200

    added another header

--- src/index.jsx
+++ src/index.jsx
@@ -3,25 +3,23 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-import './assets/stylesheets/style.css'
+import './assets/stylesheets/style.css';
 
 /****** ADD YOUR CODE AFTER THIS LINE ******/
 
 const Hello = () => (
-  <div>Hello World!</div>
+  <div>
+    <h1>Hello World!</h1>
+    <h2>This is my first time using docker.</h2>
+  </div>
 );
 
 class App extends Component {
   render() {
-    return (
-      <Hello />
-    );
+    return <Hello />;
   }
 }
 
 /****** DO NOT DELETE AFTER THIS LINE ******/
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
